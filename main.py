29c3b850d4298adf82332127f5d3cfa81b909d95
Eduardo Cabrales Alcalá <eduardo.cabrales7500@alumnos.udg.mx>
2020-09-24 09:53:17 -0500

    prueba08

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 dia = (input())
 mes = (input())
+signo = " "
 
 if (mes == "1" and dia > "20") or (mes == "2"and dia < "21"):
-    print("acuario")
+    signo = "acuario"
 elif(mes == "2" and dia > "20") or (mes == "3" and dia < "21"):
-    print("piscis")
+    signo = "piscis"
 elif(mes == "3" and dia > "20") or (mes == "4" and dia < "21"):
-    print("aries")
+    signo = "aries"
 elif(mes == "4" and dia > "20") or (mes == "5" and dia < "21"):
-    print("tauro")    
+    signo = "tauro"  
 elif(mes == "5" and dia > "20") or (mes == "6" and dia < "21"):
-    print("geminis")
+    signo = "geminis"
 elif(mes == "6" and dia > "20") or (mes == "7" and dia < "21"):
-    print("cancer")
+    signo = "cancer"
 elif(mes == "7" and dia > "20") or (mes == "8" and dia < "21"):
-    print("leo")
+    signo = "leo"
 elif(mes == "8" and dia > "20") or (mes == "9" and dia < "21"):
-    print("virgo")
+    signo = "virgo"
 elif(mes == "9" and dia > "20") or (mes == "10" and dia < "21"):
-    print("libra")
+    signo = "libra"
 elif(mes == "10" and dia > "20") or (mes == "11" and dia < "21"):
-    print("escorpio")
+    signo = "escorpio"
 elif(mes == "11" and dia > "20") or (mes == "12" and dia < "21"):
-    print("sagitario")
+    signo = "sagitario"
 elif(mes == "12" and dia > "20") or (mes == "1" and dia < "21"):
-    print("capricornio")
+    signo = "capricornio"
+print(signo)
