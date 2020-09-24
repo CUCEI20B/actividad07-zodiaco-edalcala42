ba37c476a9cfc3ecd844167322bc1fcaa416c0e7
Eduardo Cabrales Alcalá <eduardo.cabrales7500@alumnos.udg.mx>
2020-09-24 09:39:41 -0500

    Reacómodo de comparaciones

--- main.py
+++ main.py
@@ -1,64 +1,27 @@
 dia = (input())
 mes = (input())
 
-if mes == "1":
-    if dia < "21":
-        print("capricornio")
-    else:
-        print("acuario")
-elif mes == "2":
-    if dia < "20":
-        print("acuario")
-    else:
-        print("piscis")
-elif mes == "3":
-    if dia < "21":
-        print("piscis")
-    else:
-        print("aries")
-elif mes == "4":
-    if dia < "20":
-        print("aries")
-    else:
-        print("tauro")    
-elif mes == "5":
-    if dia < "21":
-        print("tauro")
-    else:
-        print("géminis")
-elif mes == "6":
-    if dia < "22":
-        print("géminis")
-    else:
-        print("cáncer")
-elif mes == "7":
-    if dia < "24":
-        print("cáncer")
-    else:
-        print("leo")
-elif mes == "8":
-    if dia < "24":
-        print("leo")
-    else:
-        print("virgo")
-elif mes == "9":
-    if dia < "23":
-        print("virgo")
-    else:
-        print("libra")
-elif mes == "10":
-    if dia < "23":
-        print("libra")
-    else:
-        print("escorpión")
-elif mes == "11":
-    if dia < "23":
-        print("escorpión")
-    else:
-        print("sagitario")
-elif mes == "12":
-    if dia < "23":
-        print("sagitario")
-    else:
-        print("acuario")
-   
+if (mes == "1" and dia > "20") or (mes == "2"and dia < "21"):
+    print("acuario")
+elif(mes == "2" and dia > "20") or (mes == "3" and dia < "21"):
+    print("piscis")
+elif(mes == "3" and dia > "20") or (mes == "4" and dia < "21"):
+    print("aries")
+elif(mes == "4" and dia > "20") or (mes == "5" and dia < "21"):
+    print("tauro")    
+elif(mes == "5" and dia > "20") or (mes == "6" and dia < "21"):
+    print("géminis")
+elif(mes == "6" and dia > "20") or (mes == "7" and dia < "21"):
+    print("cáncer")
+elif(mes == "7" and dia > "20") or (mes == "8" and dia < "21"):
+    print("leo")
+elif(mes == "8" and dia > "20") or (mes == "9" and dia < "21"):
+    print("virgo")
+elif(mes == "9" and dia > "20") or (mes == "10" and dia < "21"):
+    print("libra")
+elif(mes == "10" and dia > "20") or (mes == "11" and dia < "21"):
+    print("escorpio")
+elif(mes == "11" and dia > "20") or (mes == "12" and dia < "21"):
+    print("sagitario")
+elif(mes == "12" and dia > "20") or (mes == "1" and dia < "21"):
+    print("capricornio")
