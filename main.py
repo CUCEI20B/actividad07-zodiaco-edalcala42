94ff7b1a1ac741371a1f5628d09ed7a11f57ba44
Eduardo Cabrales Alcalá <eduardo.cabrales7500@alumnos.udg.mx>
2020-09-24 09:07:16 -0500

    prueba03

--- main.py
+++ main.py
@@ -1,65 +1,68 @@
 dia = (input())
 mes = (input())
+signo = " "
 
 if mes == "1":
     if dia < "21":
-        print("eres capricornio")
+        signo = "eres capricornio"
     else:
-        print("eres acuario")
+        signo = "eres acuario"
 elif mes == "2":
     if dia < "20":
-        print("eres acuario")
+        signo = "eres acuario"
     else:
-        print("eres piscis")
+        signo = "eres piscis"
 elif mes == "3":
     if dia < "21":
-        print("eres piscis")
+        signo = "eres piscis"
     else:
-        print("eres aries")
+        signo = "eres aries"
 elif mes == "4":
     if dia < "20":
-        print("eres aries")
+        signo = "eres aries"
     else:
-        print("eres tauro")    
+        signo = "eres tauro"  
 elif mes == "5":
     if dia < "21":
-        print("eres tauro")
+        signo = "eres tauro"
     else:
-        print("eres géminis")
+        signo = "eres géminis"
 elif mes == "6":
     if dia < "22":
-        print("eres géminis")
+        signo = "eres géminis"
     else:
-        print("eres cáncer")
+        signo = "eres cáncer"
 elif mes == "7":
     if dia < "24":
-        print("eres cáncer")
+        signo = "eres cáncer"
     else:
-        print("eres leo")
+        signo = "eres leo"
 elif mes == "8":
     if dia < "24":
-        print("eres leo")
+        signo = "eres leo"
     else:
-        print("eres virgo")
+        signo = "eres virgo"
 elif mes == "9":
     if dia < "23":
-        print("eres virgo")
+        signo = "eres virgo"
     else:
-        print("Eres Libra")
+        signo = "Eres Libra"
 elif mes == "10":
     if dia < "23":
-        print("eres libra")
+        signo ="eres libra"
     else:
-        print("eres escorpión")
+        signo = "eres escorpión"
 elif mes == "11":
     if dia < "23":
-        print("eres escorpión")
+        signo = "eres escorpión"
     else:
-        print("eres sagitario")
+        signo = "eres sagitario"
 elif mes == "12":
     if dia < "23":
-        print("eres sagitario")
+        signo = "eres sagitario"
     else:
-        print("eres acuario")
+        signo = "eres acuario"
 else:
-    print("mes no valido")    
+    signo = "mes no valido"   
+
+print (signo)
