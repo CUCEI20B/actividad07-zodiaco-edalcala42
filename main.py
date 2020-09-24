6de05c0f168b41c7d7f7e306822d7ae3cda5ec43
Eduardo Cabrales Alcalá <eduardo.cabrales7500@alumnos.udg.mx>
2020-09-24 11:45:14 -0500

    las variables mes y dia reciben enteros en lugar de cadenas

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
-dia = input()
-mes = input()
+dia = int (input())
+mes = int (input())
 
-if (mes == "1" and dia >= "20") or (mes == "2"and dia <= "18"):
+if (mes == 1 and dia >= 20) or (mes == 2 and dia <= 18):
     print("acuario")
-elif(mes == "2" and dia >= "19") or (mes == "3" and dia <= "20"):
+elif(mes == 2 and dia >= 19) or (mes == 3 and dia <= 20):
     print("piscis")
-elif(mes == "3" and dia >= "21") or (mes == "4" and dia <= "19"):
+elif(mes == 3 and dia >= 21) or (mes == 4 and dia <= 19):
     print("aries")
-elif(mes == "4" and dia >= "20") or (mes == "5" and dia <= "20"):
+elif(mes == 4 and dia >= 20) or (mes == 5 and dia <= 20):
     print("tauro")  
-elif(mes == "5" and dia >= "21") or (mes == "6" and dia <= "20"):
+elif(mes == 5 and dia >= 21) or (mes == 6 and dia <= 20):
     print("geminis")
-elif(mes == "6" and dia >= "21") or (mes == "7" and dia <= "21"):
+elif(mes == 6 and dia >= 21) or (mes == 7 and dia <= 21):
     print("cancer")
-elif(mes == "7" and dia >= "23") or (mes == "8" and dia <= "22"):
+elif(mes == 7 and dia >= 23) or (mes == 8 and dia <= 22):
     print("leo")
-elif(mes == "8" and dia >= "23") or (mes == "9" and dia <= "22"):
+elif(mes == 8 and dia >= 23) or (mes == 9 and dia <= 22):
     print("virgo")
-elif(mes == "9" and dia >= "23") or (mes == "10" and dia <= "22"):
+elif(mes == 9 and dia >= 23) or (mes == 10 and dia <= 22):
     print("libra")
-elif(mes == "10" and dia >= "23") or (mes == "11" and dia <= "21"):
+elif(mes == 10 and dia >= 23) or (mes == 11 and dia <= 21):
     print("escorpio")
-elif(mes == "11" and dia >= "22") or (mes == "12" and dia <= "21"):
+elif(mes == 11 and dia >= 22) or (mes == 12 and dia <= 21):
     print("sagitario")
-elif(mes == "12" and dia >= "22") or (mes == "1" and dia <= "19"):
+elif(mes == 12 and dia >= 22) or (mes == 1 and dia <= 19):
     print("capricornio") 
